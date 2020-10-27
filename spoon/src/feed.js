6026f27284ce839a3f493f4472190783c88c4b71
Elsa Xiaosha Huang <huangels@mail.utoronto.ca>
2020-10-27 03:59:19 -0400

    Set up feed as app's home page

---
+++ spoon/src/feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from "react-router-dom";
+
+class Feed extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Feed</p>
+      </div>
+    );
+  }
+}
+
+export default Feed;
