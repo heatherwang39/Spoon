3413752fe0a1cafd16ce288f7f1ea63e1f418a13
sun-yuhan <yuhan25@hotmail.com>
2020-10-30 14:52:17 -0400

    fixing style of files

--- spoon/src/react-components/RecipeCreate/index.js
+++ spoon/src/react-components/RecipeCreate/index.js
@@ -1,169 +1,203 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import { uid } from "react-uid";
+import { uid } from 'react-uid';
 
 import './styles.css';
 import Header from '../Header';
 
 class RecipeCreate extends React.Component {
+  state = {
+    recipeName: '',
+    servingSize: '',
+    cookTimeHrs: '',
+    cookTimeMins: '',
+    recipePhoto: '',
+    ingredients: '',
+    instructions: '',
+  };
 
-    state = {
-        recipeName: "",
-        servingSize: "",
-        cookTimeHrs: "",
-        cookTimeMins: "",
-        recipePhoto: "",
-        ingredients: "",
-        instructions: ""
-    }
+  // createRecipe=(event)=> {
+  //     event.preventDefault();
+  //     recipeName = this.state.recipeName,
+  //     servingSize = this.state.servingSize,
+  //     cookTimeHrs = this.state.cookTimeHrs ? this.state.cookTimeHrs : 0,
+  //     cookTimeMins = this.state.cookTimeMins,
+  //     recipePhoto = this.state.recipePhoto,
+  //     ingredients = this.state.ingredients,
+  //     instructions = this.state.instructions
+  // };
 
-    // createRecipe=(event)=> {
-    //     event.preventDefault();
-    //     recipeName = this.state.recipeName,
-    //     servingSize = this.state.servingSize,
-    //     cookTimeHrs = this.state.cookTimeHrs ? this.state.cookTimeHrs : 0,
-    //     cookTimeMins = this.state.cookTimeMins,
-    //     recipePhoto = this.state.recipePhoto,
-    //     ingredients = this.state.ingredients,
-    //     instructions = this.state.instructions
-    // };
+  onPhotoUpload = (event) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      this.setState({ recipePhoto: reader.result });
+    });
+    reader.readAsDataURL(event.target.files[0]);
+  };
 
-    onPhotoUpload=(event)=> {
-        const reader = new FileReader(); 
-        reader.addEventListener('load', () => { 
-            this.setState({ recipePhoto: reader.result });
-        });
-        reader.readAsDataURL(event.target.files[0]);
-    };
+  handleRecipeNameChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    this.setState({
+      recipeName: value,
+    });
+  };
 
-    handleRecipeNameChange=(event)=>{
-        const target = event.target
-        const value = target.value
-        this.setState({
-          recipeName: value
-        })
-    }
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
 
-    handleInputChange=(event)=>{
-        const target = event.target
-        const value = target.value
-        const name = target.name
-        this.setState({
-          [name]: value
-        })
-    }
-    
-    render() {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Typography variant="h2" color="secondary" gutterBottom>
+          Create Recipe
+        </Typography>
+        <Typography variant="body1" color="inherit" align="left">
+          Fields marked with * are required.
+        </Typography>
+        <Grid
+          container
+          className="createRecipeContainer"
+          justify="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item xs={8}>
+            <TextField
+              required
+              onChange={this.handleRecipeNameChange}
+              type="text"
+              name="recipeName"
+              placeholder="Ex. Pumpkin Cinnamon Rolls"
+              label="Recipe Name"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              required
+              onChange={this.handleInputChange}
+              type="number"
+              name="servingSize"
+              label="Serving Size"
+              InputProps={{
+                inputProps: {
+                  min: 1,
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              onChange={this.handleInputChange}
+              type="number"
+              name="cookTimeHrs"
+              label="Cook Time (Hours)"
+              InputProps={{
+                inputProps: {
+                  min: 0,
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              required
+              onChange={this.handleInputChange}
+              type="number"
+              name="cookTimeMins"
+              label="Cook Time (Minutes)"
+              InputProps={{
+                inputProps: {
+                  min: 1,
+                  max: 59,
+                },
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <label htmlFor="recipePhoto">
+              <input
+                accept="image/*"
+                style={{ display: 'none' }}
+                id="recipePhoto"
+                mulitple
+                type="file"
+              />
+              <Button
+                component="span"
+                onClick={this.handleRecipeNameChange}
+                variant="contained"
+                color="tertiary"
+                name="recipePhoto"
+                startIcon={<CloudUploadIcon />}
+              >
+                Upload Photos*
+              </Button>
+            </label>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              multiline
+              onChange={this.handleInputChange}
+              type="text"
+              name="ingredients"
+              label="Ingredients"
+              rows={8}
+              helperText="Please list each ingredient on a separate line."
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              multiline
+              onChange={this.handleInputChange}
+              type="text"
+              name="instructions"
+              label="Instructions"
+              rows={10}
+              helperText="Please list each instruction on a separate line."
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
 
-      return (
-        <div>
-            <Header/>
-            <Typography variant="h2" color="secondary" gutterBottom>Create Recipe</Typography>
-            <Typography variant="body1" color="inherit" align="left">Fields marked with * are required.</Typography>
-            <Grid container className="createRecipeContainer" justify="center" alignItems="center" spacing={1}>
-                <Grid item xs={8}>
-                    <TextField required
-                        onChange={this.handleRecipeNameChange}
-                        type="text"
-                        name="recipeName"
-                        placeholder="Ex. Pumpkin Cinnamon Rolls"
-                        label="Recipe Name"
-                        variant="outlined" fullWidth />
-                </Grid> 
-                <Grid item xs={4}>
-                    <TextField required 
-                        onChange={this.handleInputChange}
-                        type="number"
-                        name="servingSize"
-                        label="Serving Size"
-                        InputProps={{
-                            inputProps: { 
-                                min: 1
-                            }
-                        }}
-                        variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={4}>
-                    <TextField 
-                        onChange={this.handleInputChange}
-                        type="number"
-                        name="cookTimeHrs"
-                        label="Cook Time (Hours)"
-                        InputProps={{
-                            inputProps: { 
-                                min: 0
-                            }
-                        }}
-                        variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={4}>
-                    <TextField required 
-                        onChange={this.handleInputChange}
-                        type="number"
-                        name="cookTimeMins"
-                        label="Cook Time (Minutes)"
-                        InputProps={{
-                            inputProps: { 
-                                min: 1,
-                                max:59
-                            }
-                        }}
-                        variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={4}>
-                    <label htmlFor="recipePhoto">
-                        <input accept="image/*"
-                            style={{ display: "none" }} 
-                            id="recipePhoto"  
-                            mulitple  
-                            type="file"/> 
-                        <Button component="span" onClick={this.handleRecipeNameChange}
-                            variant="contained"
-                            color="tertiary"
-                            name="recipePhoto"
-                            startIcon={<CloudUploadIcon />}>
-                            Upload Photos*
-                        </Button>
-                    </label>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField required multiline
-                        onChange={this.handleInputChange}
-                        type="text"
-                        name="ingredients"
-                        label="Ingredients"
-                        rows={8}
-                        helperText="Please list each ingredient on a separate line."
-                        variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField required multiline
-                        onChange={this.handleInputChange}
-                        type="text"
-                        name="instructions"
-                        label="Instructions"
-                        rows={10}
-                        helperText="Please list each instruction on a separate line."
-                        variant="outlined" fullWidth />
-                </Grid>
-                
-                <Grid item xs={12}>
-                    <Button onClick={this.createRecipe}
-                        variant="contained"
-                        color="primary"
-                        onClick={this.addRecipe}
-                        name="addRecipeButton">
-                        Add Recipe
-                    </Button>
-                </Grid>
-            </Grid>
-        </div>
-      );
-    }
+          <Grid item xs={12}>
+            <Button
+              onClick={this.createRecipe}
+              variant="contained"
+              color="primary"
+              onClick={this.addRecipe}
+              name="addRecipeButton"
+            >
+              Add Recipe
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default RecipeCreate;
--- spoon/src/react-components/RecipePopUp/index.js
+++ spoon/src/react-components/RecipePopUp/index.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
-import Grid from "@material-ui/core/Grid";
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import CloseIcon from '@material-ui/icons/Close';
@@ -12,7 +12,8 @@ import './styles.css';
 
 // BELOW IS EXAMPLE OF HOW TO USE IT
 
-{/*
+{
+  /*
 
 <RecipePopup
     title="recipe title"
@@ -28,7 +29,8 @@ onClick={setOpenPopup(true)}
 +
 import RecipePopup from '../RecipePopup';
 
-*/}
+*/
+}
 
 class RecipePopup extends React.Component {
   render() {
@@ -39,40 +41,53 @@ class RecipePopup extends React.Component {
       numLikes,
       children,
       openPopup,
-      setOpenPopup
+      setOpenPopup,
     } = this.props;
 
     return (
       <div>
-        <Dialog open={openPopup}
-        scroll="paper">
-          <DialogTitle >
+        <Dialog open={openPopup} scroll="paper">
+          <DialogTitle>
             <div>
               <Typography variant="h3" color="primary" align="left">
                 {title}
               </Typography>
-              <Button onClick={setOpenPopup(false)}
+              <Button
+                onClick={setOpenPopup(false)}
                 variant="contained"
                 color="primary"
-                name="closePopup">
+                name="closePopup"
+              >
                 <CloseIcon />
               </Button>
             </div>
           </DialogTitle>
           <DialogContent dividers>
-              <Typography variant="h5" color="secondary" align="left">By: {author}</Typography>
-              <img src={require(photo)}/>
-              <Grid container className="favouritesContainer" justify="flex-end">
-                <Grid item>
-                  {/* TO FIX, REPLACE ONCLICK WITH APPROPRIATE ACTION*/}
-                  {/* <Button color="primary" onClick={handleClickOpen}><FavoriteIcon /></Button> */}
-                  <Button color="primary"><FavoriteIcon /></Button>
-                </Grid>
-                <Grid item>
-                  <Typography variant="subtitle1" color="textPrimary" align="right">{numLikes}</Typography>
-                </Grid>
+            <Typography variant="h5" color="secondary" align="left">
+              By: {author}
+            </Typography>
+            <img src={require(photo)} />
+            <Grid container className="favouritesContainer" justify="flex-end">
+              <Grid item>
+                {/* TO FIX, REPLACE ONCLICK WITH APPROPRIATE ACTION*/}
+                {/* <Button color="primary" onClick={handleClickOpen}><FavoriteIcon /></Button> */}
+                <Button color="primary">
+                  <FavoriteIcon />
+                </Button>
               </Grid>
-              <Typography variant="body1" color="textSecondary" align="left">{children}</Typography>
+              <Grid item>
+                <Typography
+                  variant="subtitle1"
+                  color="textPrimary"
+                  align="right"
+                >
+                  {numLikes}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Typography variant="body1" color="textSecondary" align="left">
+              {children}
+            </Typography>
           </DialogContent>
         </Dialog>
       </div>
