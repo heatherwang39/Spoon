62ffccc17396670f18da8e999f1071bce8986283
Elsa Xiaosha Huang <huangels@mail.utoronto.ca>
2020-11-05 04:16:50 -0500

    Started UserProfile UI using material-ui

--- spoon/src/react-components/UserProfile/index.js
+++ spoon/src/react-components/UserProfile/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-// import './styles.css';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import './styles.css';
 
 // import Thumbnail from '../Thumbnail';
 import Header from '../Header';
@@ -10,33 +15,48 @@ class UserProfile extends React.Component {
     return (
       <div>
         <Header />
-        <div>
-          <p>xXfire_dragonXx</p>
-          <p>420 folowers</p>
-        </div>
-        <div className="tab">
-          <button
-            className="tablinks"
-            type="button"
-            onClick="openTab(event, 'posts')"
-          >
-            Posts
-          </button>
-          <button
-            className="tablinks"
-            type="button"
-            onClick="openTab(event, 'likes')"
-          >
-            Likes
-          </button>
-          <div id="posts" className="tabcontent">
-            posts some posts and some more posts
+        <div className="userprofile-profile">
+          <div>
+            <Typography
+              className="userprofile-username"
+              variant="h2"
+              color="secondary"
+              align="left"
+            >
+              xXfire_dragonXx
+            </Typography>
+            <Typography
+              className="userprofile-follower-count"
+              variant="h5"
+              color="secondary"
+              align="left"
+            >
+              420 followers
+            </Typography>
+            <Button
+              className="userprofile-button"
+              variant="contained"
+              color="secondary"
+            >
+              FOLLOW
+            </Button>
           </div>
-          <div id="likes" className="tabcontent">
-            liked posts and more liked posts and stuff
-          </div>
-          {/* <Thumbnail /> */}
+          <AppBar position="static" color="secondary">
+            <Tabs>
+              <Tab label="Recipes" />
+              <Tab label="Likes" />
+            </Tabs>
+          </AppBar>
         </div>
+        {/* <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel> */}
       </div>
     );
   }
