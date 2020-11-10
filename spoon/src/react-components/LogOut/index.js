c98695a279afdaadd9a5befc712f4bde7bb92f9b
joyce-ma <joycema.ma@mail.utoronto.ca>
2020-11-09 19:51:17 -0500

    log out

--- spoon/src/react-components/LogOut/index.js
+++ spoon/src/react-components/LogOut/index.js
@@ -9,22 +9,43 @@
   class LogOut extends React.Component {
     state = {
       message: "",
+      password: "",
+      username: "",
       header: (<Header state={this.props.appState} />)
     }
 
     componentDidMount(){
-      const newState = {username: "", userMode: "guest"}
+      const newState = {username: "guest", userMode: "guest"}
       this.setState({
         header: (<Header state={newState} />)
       })
     }
 
-    // check = () => {
-    //   success
-    // }
+    handleInputChange = (event) => {
+      const name = event.target.name;
+      this.setState({
+        [name]: event.target.value,
+      });
+    };
+
+    check = () => {
+      if ((this.state.username === "user" && this.state.password === "user")
+      || (this.state.username === "admin" && this.state.password === "admin")) {
+        this.success()
+      }
+      else {
+        this.setState({message:
+          "You have entered incorrect credentials."}
+        )
+      }
+      this.setState({
+        username: "", 
+        password: ""
+      })
+    }
 
     success = () => {
-      const newState = {username: "user1", userMode: "user"}
+      const newState = {username: this.state.username, userMode: this.state.username}
       this.setState({
         header: (<Header state={newState} />)
       })
@@ -40,43 +61,43 @@
           <Grid container justify="center" alignItems="center" spacing={1}>
             <Grid item xs={5}>
               <TextField
-                  //  value={searchedKeyword}
-                  //  onChange={handleInputChange}
+                value={this.state.username}
+                onChange={this.handleInputChange}
                 type="text"
-                name=""
-                placeholder="e.g. user1"
+                name="username"
+                placeholder="e.g. user"
                 label="Username"
                 variant="outlined"
                 fullWidth
               />
             </Grid>
 
-          <Grid item xs={5}>
-            <TextField
-                //  value={searchedKeyword}
-                //  onChange={handleInputChange}
-              name=""
-              placeholder="e.g. user1"
-              label="Password"
-              variant="outlined"
-              type="password"
-              fullWidth
-            />
-          </Grid>
+            <Grid item xs={5}>
+              <TextField
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                name="password"
+                placeholder="e.g. user"
+                label="Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+              />
+            </Grid>
 
-           <Grid item xs={12}>
-             <Button
-               onClick={this.success}
-               variant="outlined"
-               color="secondary"
-               size="large"
-             >
-               Sign In
-             </Button>
-             </Grid>
-           <Grid item xs={12}>
-           <Typography>{this.state.message}</Typography>
-           </Grid>
+            <Grid item xs={12}>
+              <Button
+                onClick={this.check}
+                variant="outlined"
+                color="secondary"
+                size="large"
+              >
+                Sign In
+              </Button>
+              </Grid>
+            <Grid item xs={12}>
+            <Typography>{this.state.message}</Typography>
+            </Grid>
 
          </Grid>
        </div>
