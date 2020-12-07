335caec2c87649acb1a8b190b2fa48311c8ab753
unknown <heatherwang39@gmail.com>
2020-12-07 18:07:46 -0500

    add user schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 // User mongoose model
 
 const mongoose = require("mongoose");
+// const validator = require('validator')
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("User", {
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -13,7 +15,7 @@ const User = mongoose.model("User", {
   password: {
     type: String,
     required: true,
-    minlength: 1,
+    minlength: 6,
   },
   isAdmin: {
     type: Boolean,
@@ -45,4 +47,47 @@ const User = mongoose.model("User", {
   },
 });
 
-module.exports = { User };
+
+UserSchema.pre('save', function(next) {
+	const user = this; // binds this to User document instance
+
+	// checks to ensure we don't hash password more than once
+	if (user.isModified('password')) {
+		// generate salt and hash the password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next()
+	}
+})
+
+// Allows us to find a User document by comparing the hashed password
+//  to a given one, for example when logging in.
+UserSchema.statics.findByUsernamePassword = function(username, password) {
+	const User = this // binds this to the User model
+
+	// First find the user by their username
+	return User.findOne({ username: username }).then((user) => {
+		if (!user) {
+			return Promise.reject()  // a rejected promise
+		}
+		// if the user exists, make sure their password is correct
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, result) => {
+				if (result) {
+					resolve(user)
+				} else {
+					reject()
+				}
+			})
+		})
+	})
+}
+
+
+const User = mongoose.model('User', UserSchema)
+module.exports = { User }
